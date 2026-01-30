014288bfee7de68dac75f3141399b4e79440b4e0
witaminabc <witaminabc@mail.ru>
2026-01-30 10:57:38 +0300

    Обновил .gitignore: добавил игнорирование конкретных плагинов

---
+++ wp-content/advanced-cache.php
@@ -0,0 +1,10 @@
+<?php
+/* PageSpeed Ninja Caching */
+defined('ABSPATH') || die();
+define('PAGESPEEDNINJA_CACHE_DIR', '/home/users/j/j1134838/domains/fresh.rosaitdemo.ru/wp-content/plugins/psn-pagespeed-ninja/cache');
+define('PAGESPEEDNINJA_CACHE_PLUGIN', '/home/users/j/j1134838/domains/fresh.rosaitdemo.ru/wp-content/plugins/psn-pagespeed-ninja');
+define('PAGESPEEDNINJA_CACHE_RESSDIR', '/home/users/j/j1134838/domains/fresh.rosaitdemo.ru/wp-content/plugins/psn-pagespeed-ninja/ress');
+define('PAGESPEEDNINJA_CACHE_DEVICEDEPENDENT', true);
+define('PAGESPEEDNINJA_CACHE_TTL', 86400);
+define('PAGESPEEDNINJA_CACHE_GZIP', 1);
+include '/home/users/j/j1134838/domains/fresh.rosaitdemo.ru/wp-content/plugins/psn-pagespeed-ninja/public/advanced-cache.php';
